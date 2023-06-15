9efaef138bd17606085304d15b54c6b585b13e8d
John Novakowski <jnovakowski797@gmail.com>
2023-06-14 20:12:12 -0400

    feature of including information on marks and server differentiation

--- server/index.ts
+++ server/index.ts
@@ -3,11 +3,13 @@ import express from 'express';
 import morgan from 'morgan';
 import path from 'path';
 import axios from 'axios';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../dist')));
 
