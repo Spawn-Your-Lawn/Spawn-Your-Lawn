c4a905ba5f46bbd093b02ed8518b32281e504ece
afrodynamic <33985296+afrodynamic@users.noreply.github.com>
2023-06-16 14:04:48 -0400

    fix: resolve merge conflict for PR #5

--- client/src/features/storeDetails/StoreDetails.tsx
+++ client/src/features/storeDetails/StoreDetails.tsx
@@ -37,3 +37,27 @@ export const StoreDetails: FC<StoreDetailsProps> = ({ locationTags }) => {
     </div>
   );
 };
+
+  return (
+    <div>
+      <div>
+        {locationTags.name && <p>{locationTags.name}</p>}
+      </div>
+      <div>
+        {locationTags['addr:housenumber'] && <p>{locationTags['addr:housenumber']}</p>}
+      </div>
+      <div>
+        {locationTags['addr:street'] && <p>{locationTags['addr:street']}</p>}
+      </div>
+      <div>
+        {locationTags['addr:city'] && <p>{locationTags['addr:city']}</p>}
+      </div>
+      <div>
+        {locationTags['addr:postcode'] && <p>{locationTags['addr:postcode']}</p>}
+      </div>
+      <div>
+        {locationTags['phone'] && <p>{locationTags['phone']}</p>}
+      </div>
+    </div>
+  );
+};
